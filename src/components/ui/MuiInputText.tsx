c39e1c09947fffa91f21ec3505fabc1e308783a6
松浦　昂生 <kouki19991222@icloud.com>
2023-07-23 17:06:51 +0900

    入力フォームを追加

---
+++ src/components/ui/MuiInputText.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { TextField } from '@mui/material';
+
+const MuiTextField = () => <TextField label="Standard" variant="standard" />;
+
+export default MuiTextField;
